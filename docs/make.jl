4264c3ded4ef705db1649f94861f11ef279bb083
Ni Wang <125902905+gnawin@users.noreply.github.com>
2025-09-19 11:02:19 +0200

    Fix docs (#49)
    * Fix docs
    
    * Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,6 @@ DocMeta.setdocmeta!(
     recursive = true,
 )
 
-const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
 const numbered_pages = [
     file for file in readdir(joinpath(@__DIR__, "src")) if
     file != "index.md" && splitext(file)[2] == ".md"
@@ -17,7 +16,6 @@ const numbered_pages = [
 makedocs(;
     modules = [NearOptimalAlternatives],
     authors = "",
-    repo = "https://github.com/TulipaEnergy/NearOptimalAlternatives.jl/blob/{commit}{path}#{line}",
     sitename = "NearOptimalAlternatives.jl",
     format = Documenter.HTML(;
         canonical = "https://TulipaEnergy.github.io/NearOptimalAlternatives.jl",
